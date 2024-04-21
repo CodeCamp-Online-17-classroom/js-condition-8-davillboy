a2c5e5a13fd17d817a76d2ad82ed018565fe2aa6
davillboy <71083807+davillboy@users.noreply.github.com>
2024-04-22 01:49:29 +0700

    first change

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
 // write code here
+  <script>
+        let score = prompt("Enter your score:");
+
+        let grade;
+        if (score >= 80) {
+            grade = 'A';
+        } else if (score >= 70) {
+            grade = 'B';
+        } else if (score >= 60) {
+            grade = 'C';
+        } else if (score >= 50) {
+            grade = 'D';
+        } else {
+            grade = 'F';
+        }
+
+        alert("Your grade is: " + grade);
+
+  </script>  
+
